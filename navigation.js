805e3434217ba2e0c62f9c1e971ab2bf564ad2d4
abdul-busybrain <awwalhajine@gmail.com>
2025-02-16 17:49:23 +0100

    Navigation functionality

---
+++ navigation.js
@@ -0,0 +1,34 @@
+function updateNavbar() {
+  const user = JSON.parse(sessionStorage.getItem("user"));
+  const navbar = document.getElementById("navbar");
+
+  if (user) {
+    const role = user.role;
+    let links = "";
+
+    if (role === "admin") {
+      links = `
+              <li><a href'./index.html'>My Dashboard</a></li>
+              <li><a href'../manage/students.html'>Manage Students</a></li>
+              <li><a href'../manage/courses.html'>Manage Courses</a></li>
+              <li><a href'../auth/logout.html'>Logout</a></li>
+              `;
+    } else if (role === "student") {
+      links`
+          
+          <li><a href'./index.html'>Dashboard</a></li>
+          <li><a href'../coursework/index.html'>View Course Work</a></li>
+          <li><a href'../auth/logout.html'></a></li>
+          `;
+    } else {
+      links = `
+          <li><a href="./loginPage.html">Login</a></li>
+          <li><a href="./registerPage.html">Register</a></li>
+        `;
+    }
+  }
+
+  navbar.innerHTML = `<ul>${links}</ul>`;
+}
+
+window.onload = updateNavbar;
