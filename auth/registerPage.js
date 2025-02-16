376a038697f11dfb7c81ef249fc4dd64c635d7fb
abdul-busybrain <awwalhajine@gmail.com>
2025-02-16 20:29:35 +0100

    Fixed register page

--- auth/registerPage.js
+++ auth/registerPage.js
@@ -1,22 +1,24 @@
-document.querySelector("form").addEventListener("submit", function (e) {
-  e.preventDefault();
+document
+  .getElementById("registerForm")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
 
-  const fullName = document.getElementById("fullName").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  const confirmPassword = document.getElementById("confirmPassword").value;
-  const role = document.getElementById("role").value;
+    const fullName = document.getElementById("fullName").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+    const role = document.getElementById("role").value;
 
-  if (password !== confirmPassword) {
-    alert("Passwords do not match");
-    return;
-  }
+    if (password !== confirmPassword) {
+      alert("Passwords do not match");
+      return;
+    }
 
-  const users = JSON.parse(localStorage.getItem("users")) || [];
-  const newUser = { fullName, email, password, role };
-  users.push(newUser);
-  localStorage.setItem("users", JSON.stringify(users));
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const newUser = { fullName, email, password, role };
+    users.push(newUser);
+    localStorage.setItem("users", JSON.stringify(users));
 
-  alert(`${fullName} your ${role} account has been created successfully`);
-  window.location.href = "./loginPage.html";
-});
+    alert(`${fullName} your ${role} account has been created successfully`);
+    window.location.href = "./loginPage.html";
+  });
